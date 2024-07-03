5536feefe4194c5f3f21277663891c0507983e2b
DESKTOP-8IENF4O\User <yourdeveloper@mail.ru>
2024-07-03 16:17:16 +0700

    add build

--- src/features/CubeGame/index.ts
+++
@@ -1,5 +0,0 @@
-export {GameResult} from './ui/GameResult/GameResult';
-export {BetSizePanel} from './ui/BetSizePanel/BetSizePanel';
-export {BetOptionsPanel} from './ui/BetOptionsPanel/BetOptionsPanel';
-
-export {useCubeStore} from './model/store/CubeStore';
